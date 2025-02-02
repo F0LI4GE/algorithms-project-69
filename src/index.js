15d531c4de6afe2cedd38621578a67a770a1b271
Alexandr Shavlokhov <alexandr.shavlokhov@gmail.com>
2025-02-02 19:35:43 +0300

    refactor indexing functions to calculate TF-IDF and improve document ranking

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
+const parseDocumentContent = (document) => document.text.split(' ').map((token) => token.match(/\w+/g)).flat();
+
+export const calculateTF = (document) => {
+  const words = parseDocumentContent(document);
+  const wordCounts = {};
+
+  for (let i = 0; i < words.length; i += 1) {
+    const word = words[i];
+    wordCounts[word] = (wordCounts[word] || 0) + 1;
+  }
+
+  const termFreqs = {};
+
+  Object.keys(wordCounts).forEach((word) => {
+    termFreqs[word] = wordCounts[word] / words.length;
+  });
+
+  return termFreqs;
+};
+
+export const calculateIDF = (documents) => {
+  const docCount = documents.length;
+  const wordDocCount = {};
+
+  documents.forEach((doc) => {
+    const words = new Set(doc.text.match(/\w+/g));
+    words.forEach((word) => {
+      wordDocCount[word] = (wordDocCount[word] || 0) + 1;
+    });
+  });
+
+  const idf = {};
+  Object.keys(wordDocCount).forEach((word) => {
+    idf[word] = Math.log(docCount / wordDocCount[word]);
+  });
+
+  return idf;
+};
+
+export const invertIndex = (documents) => {
+  const index = {};
+
+  const IDF = calculateIDF(documents);
+
+  for (let i = 0; i < documents.length; i += 1) {
+    const doc = documents[i];
+    const words = doc.text.match(/\w+/g);
+
+    const TF = calculateTF(doc);
+
+    for (let j = 0; j < words.length; j += 1) {
+      const word = words[j];
+      const wordFreq = TF[word];
+      const wordInverseDocumentFreq = IDF[word];
+      const wordTfIdf = wordFreq * wordInverseDocumentFreq;
+
+      if (!index[word]) {
+        index[word] = {};
+      }
+      index[word][doc.id] = wordTfIdf;
+    }
+  }
+
+  return index;
+};
+
 export default (documents, phrase) => {
   const ranks = {};
   const parsedTargets = phrase.match(/\w+/g);
@@ -18,25 +84,4 @@ export default (documents, phrase) => {
   }
 
   return Object.entries(ranks).sort((e1, e2) => e2[1] - e1[1]).map((entry) => entry[0]);
-};
-
-export const invertIndex = (documents) => {
-  const index = {};
-
-  for (let i = 0; i < documents.length; i += 1) {
-    const doc = documents[i];
-    const words = doc.text.match(/\w+/g);
-
-    for (let j = 0; j < words.length; j += 1) {
-      const word = words[j];
-
-      if (!index[word]) {
-        index[word] = [doc.id];
-      } else if (!index[word].includes(doc.id)) {
-        index[word].push(doc.id);
-      }
-    }
-  }
-
-  return index;
 };
