aa83b4ce882ca814c52a3ab4c436d63a717a94ed
Alexandr Shavlokhov <alexandr.shavlokhov@gmail.com>
2025-02-02 13:21:52 +0300

    improve ranking and add additional test cases

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -3,10 +3,12 @@ import search from '../index.js';
 const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
 const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
-const docs = [doc1, doc2, doc3];
+const doc4 = { id: 'doc4', text: "I'm your shooter." };
+const doc5 = { id: 'doc5', text: "I'm your shooter. shoot shoot shoot shoot shoot" };
+const docs = [doc1, doc2, doc3, doc4, doc5];
 
 test('search', () => {
-  expect(search(docs, 'shoot')).toStrictEqual(['doc1', 'doc2']);
+  expect(search(docs, 'shoot')).toStrictEqual(['doc5', 'doc2', 'doc1']);
   expect(search(docs, 'DOESNOTEXIST')).toStrictEqual([]);
   expect(search(docs, 'pint!')).toStrictEqual(['doc1']);
   expect(search(docs, 'pint')).toStrictEqual(['doc1']);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 export default (documents, target) => {
-  const result = new Set();
+  const ranks = {};
   const parsedTarget = target.match(/\w+/g)[0];
 
   for (let i = 0; i < documents.length; i += 1) {
-    const words = documents[i].text.split(' ').map((token) => token.match(/\w+/g)).flat();
+    const document = documents[i];
+    const words = document.text.split(' ').map((token) => token.match(/\w+/g)).flat();
 
     for (let j = 0; j < words.length; j += 1) {
       if (words[j] === parsedTarget) {
-        result.add(documents[i].id);
+        if (ranks[document.id]) {
+          ranks[document.id] += 1;
+        } else {
+          ranks[document.id] = 1;
+        }
       }
     }
   }
 
-  return [...result.values()];
+  return Object.entries(ranks).sort((e1, e2) => e2[1] - e1[1]).map((entry) => entry[0]);
 };
